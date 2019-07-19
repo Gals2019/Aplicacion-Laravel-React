536ac2825c8808534a0ac950d45b551a0a6258f9
gals <developergals@gmail.com>
2019-07-19 11:52:53 -0300

    Agregando JWT y su configuracion basica

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+    Route::post('recover', 'AuthController@recover');
+    Route::group(['middleware' => ['jwt.auth']], function() {
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+        Route::get('logout', 'AuthController@logout');
+        Route::get('test', function(){
+            return response()->json(['foo'=>'bar']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+        });
+        
 });
